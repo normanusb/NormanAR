2b082124db4bf208cbe1c56afb991b3afac21ff6
Norman Lista <Normanusb@gmail.com>
2017-04-01 03:23:29 +0200

    bullet script

---
+++ Script/Gun.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform spawnObject;
+	
+	private void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject go = Instantiate(bulletPrefab,spawnObject.position,transform.rotation) as GameObject;
+            go.GetComponent<Rigidbody>().AddForce(transform.forward * 30,ForceMode.Impulse);
+        }
+	}
+}
